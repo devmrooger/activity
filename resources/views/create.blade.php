cf84d3d60b50727613248b548c485e74337d1578
Mauro Roger <mauro.roger@kodigos.com.br>
2020-12-09 15:01:16 -0400

    Personalização Create Activity

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,12 +1,9 @@
 @extends('templates.template')
 
 @section('content')
-    <h1 class="text-center">@if(isset($activity))Editar @else Cadastrar @endif</h1>
+    <h1 class="col-12 text-center">@if(isset($activity))Editar @else Cadastrar @endif</h1><hr>
 
-    
-
-    <div class=" container col-8 m-auto">
-
+    <div class="container col-8 m-auto"><br>
         @if (isset($errors) && count($errors) > 0)
             <div class="text-center mt-4 mb-4 p-2 alert-danger">
                 @foreach ($errors->all() as $erro)
@@ -14,7 +11,6 @@
                 @endforeach
             </div>
         @endif
-
         @if (isset($activity))
             <form id="formEdit" name="formEdit" action="{{url("activity/$activity->id")}}" method="post">
                 @method('PUT')
@@ -22,44 +18,62 @@
             <form id="formCad" name="formCad" action="{{url("activity")}}" method="post">
         @endif
             @csrf
-                <select class="form-control"name="user_id" id="user_id" required>
-                    <option value="{{$activity->relUsers->id ?? ''}}">{{$activity->relUsers->name ?? 'Nome Atarefado'}}</option>
-                        @foreach ($users as $user)
-                            <option value="{{$user->id}}">{{$user->name}}</option>
-                        @endforeach
-                </select><br>
-                <input class="form-control" id="categoria" name="categoria" type="text" placeholder="Categoria" value="{{$activity->categoria ?? ''}}" required><br>
-                <input class="form-control" id="descricao" name="descricao" type="text" placeholder="Descrição" value="{{$activity->descricao ?? ''}}" required><br>
-
-                {{-- <div class="custom-control custom-switch ">
-                    <input id="status" name="status"  type="checkbox" class="custom-control-input" value="{{$activity->status ?? 0}}">
-                    <label class="custom-control-label" for="status">Concluído</label>
-                </div> --}}
-
-                <select class="form-control" name="status" id="status" placeholder="Status" required>
-                    <option value="{{$activity->status ?? ''}}">
-                        @if (isset($activity))
-                            @if ($activity->status === 0)
-                                Não Concluído
-                            @else
-                                Concluído
-                            @endif
-                        @else
-                            Status
-                        @endif
-                    </option>
-                    <option value="0">Não Concluído</option>
-                    <option value="1">Concluído</option>
-                </select>
-
-                <br>
-                <input class="form-control" id="nivel" name="nivel" type="text" placeholder="Nível" value="{{$activity->nivel ?? ''}}" required><br>
-                <input class="btn btn-primary" type="submit" value="@if(isset($activity)) Salvar @else Cadastrar @endif"><br>
+                <div class="row">
+                    <div class="col">
+                        <label for="user_id">Atarefado</label>
+                        <select class="form-control"name="user_id" id="user_id" required>
+                            <option value="{{$activity->relUsers->id ?? ''}}">{{$activity->relUsers->name ?? 'Nome Atarefado'}}</option>
+                                @foreach ($users as $user)
+                                    <option value="{{$user->id}}">{{$user->name}}</option>
+                                @endforeach
+                        </select>
+                    </div>
+                    <div class="col">
+                        <label for="categoria">Categoria</label>
+                        <input class="form-control" id="categoria" name="categoria" type="text" placeholder="Categoria" value="{{$activity->categoria ?? ''}}" required><br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label for="descricao">Descrição</label>
+                        <input class="form-control" id="descricao" name="descricao" type="text" placeholder="Descrição" value="{{$activity->descricao ?? ''}}" required><br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3">
+                        <label for="status">Status</label>
+                        <select class="form-control" name="status" id="status" placeholder="Status" required>
+                            <option value="{{$activity->status ?? ''}}">
+                                @if (isset($activity))
+                                    @if ($activity->status === 0)
+                                        Não Concluído
+                                    @else
+                                        Concluído
+                                    @endif
+                                @else
+                                    Status
+                                @endif
+                            </option>
+                            <option value="0">Não Concluído</option>
+                            <option value="1">Concluído</option>
+                        </select>
+                    </div>
+                    <div class="col-3">
+                        <label for="nivel">Nível</label>
+                        <input class="form-control" id="nivel" name="nivel" type="text" placeholder="Nível" value="{{$activity->nivel ?? ''}}" required><br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6 mr-auto">
+                        <a href="{{url("activity")}}">
+                            <button class="btn btn-dark">Voltar</button>
+                        </a>
+                    </div>
+                    <div class="col-4"></div>
+                    <div class="col-auto">
+                        <input class="btn btn-primary" type="submit" value="@if(isset($activity)) Salvar @else Cadastrar @endif"><br>
+                    </div>
+                </div>
             </form>
-            <div class="container">
-                <a href="{{url("activity")}}">
-                    <button class="btn btn-dark">Voltar</button>
-                </a>
-            </div>
     </div>
 @endsection
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -6,17 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ATIVIDADES</title>
 
-    <link rel="stylesheet" href="{{url("assets/bootstrap/css/bootstrap.min.css")}}">
-    <script src="{{url("assets/bootstrap/js/bootstrap.bundle")}}"></script>
+    {{-- <link rel="stylesheet" href="{{url("assets/bootstrap/css/bootstrap.min.css")}}">
+    <script src="{{url("assets/bootstrap/js/bootstrap.bundle")}}"></script> --}}
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script> --}}
     
     {{-- <link rel="stylesheet" href="{{url("assets/bootstrap/css/bootstrap.min.css")}}"> --}}
-    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     {{-- <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css"> --}}
 </head>
 <body>
